4d328ae903b54bdb2551d32efac4a205d32888c8
Gino Messmer <git@ginomessmer.me>
2019-01-03 15:33:05 +0100

    Add blank expenses page

---
+++ ml.Doki/Views/ExpensesPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using ml.Doki.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace ml.Doki.Views
+{
+    public sealed partial class ExpensesPage : Page
+    {
+        public ExpensesViewModel ViewModel { get; } = new ExpensesViewModel();
+
+        public ExpensesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
